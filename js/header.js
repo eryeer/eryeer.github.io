141605c475c691c22043a9b9e8a77fce62700a4d
eryeer <eryeer@163.com>
2017-07-24 22:26:36 +0800

    updated on 2017/7/24

--- js/header.js
+++ js/header.js
@@ -9,5 +9,19 @@ jQuery(function ($) {
         .on("mouseout",function(){
             $(this).css("background-color", "#111");
         });
+    $("#btngit")
+        .on("click",function(){
+            $(".img-cover").fadeOut(1000,function(){
+                window.location='https://github.com/eryeer';
+            });
+
+        });
+    $("#btnlinks")
+        .on("click",function(){
+            $(".img-cover").fadeOut(1000,function(){
+                window.location='./links.html';
+            });
+
+        });
 
 });
