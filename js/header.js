7e0e085900c9b4f8757ae31c5c334e7042b98fce
eryeer <eryeer@163.com>
2017-07-26 14:00:19 +0800

    Updated on 2017-7-26

--- js/header.js
+++ js/header.js
@@ -4,10 +4,46 @@
 jQuery(function ($) {
     $(".head li")
         .on("mouseover", function () {
-            $(this).css("background-color", "#666");
+            $(this).css({
+                "background-color":"#666",
+                "cursor":"pointer"
+            });
         })
         .on("mouseout",function(){
             $(this).css("background-color", "#111");
+        });
+    $(".head li:eq(0)")
+        .on("click",function(){
+            if(document.title=='Home'){
+                location='#';
+            }else{
+                location='./index.html';
+            }
+        });
+    $(".head li:eq(1)")
+        .on("click",function(){
+            window.open('https://github.com/eryeer');
+
+        });
+    $(".head li:eq(2)")
+        .on("click",function(){
+            if(document.title=='Links'){
+                location='#';
+            }else{
+                location='./links.html';
+            }
+        });
+    $(".head li:eq(3)")
+        .on("click",function(){
+            if(document.title=='Contact Me'){
+                location='#';
+            }else{
+                location='./contact.html';
+            }
+        });
+    $(".head a")
+        .on("click",function(){
+            return false;
         });
     $("#btngit")
         .on("click",function(){
